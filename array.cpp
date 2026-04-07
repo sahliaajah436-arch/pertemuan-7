a67164114bca873ef88922fee4532b4bcecc637f
sahliaajah436-arch <sahliaajah436@gmail.com>
2026-04-07 18:31:23 +0700

    menambahkan perulangan for untuk input array datamu

--- array.cpp
+++ array.cpp
@@ -37,5 +37,12 @@ int main()
     cout << "data kelima = " << datamu[4] << endl;
     cout << endl;
 
-    
+    cout << "Input Array Menggunakan For" << endl;
+    //Mengisi data melalui perulangan
+    for (int i = 0; i <= 4; i++)
+    {
+        cout << "Isikan data ke-" << (i + 1) << " : ";
+        cin >> datamu[i];
+    }
+   
 }
