a992dc666988e3d712eeb5378465403bf722f6b2
sahliaajah436-arch <sahliaajah436@gmail.com>
2026-04-07 18:20:20 +0700

    mengubah nilai array dataku index ke-1 menjadi 200

--- array.cpp
+++ array.cpp
@@ -12,4 +12,8 @@ int main()
     // Cara akses (membaca atau mengambil isi array)
     cout << "Isi data kedua : " << dataku[1] << endl;
 
+    // Mengisi array
+    dataku[1] = 200;
+    cout << "Isi data kedua setelah diubah : " << dataku[1] << endl;
+   
    
