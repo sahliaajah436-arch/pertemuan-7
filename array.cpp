1388e71acb4dc0039fcc7ffa8c1ac93d437adb34
sahliaajah436-arch <sahliaajah436@gmail.com>
2026-04-07 18:29:44 +0700

    menampilkan isi array datamu secara manual

--- array.cpp
+++ array.cpp
@@ -16,5 +16,26 @@ int main()
     dataku[1] = 200;
     cout << "Isi data kedua setelah diubah : " << dataku[1] << endl;
    
-   
+    // Mengisi data dengan perintah satu persatu
+    cout << "Isikan data pertama : ";
+    cin >> datamu[0];
+    cout << "Isikan data kedua : ";
+    cin >> datamu[1];
+    cout << "Isikan data ketiga : ";
+    cin >> datamu[2];
+    cout << "Isikan data keempat : ";
+    cin >> datamu[3];
+    cout << "Isikan data kelima : ";
+    cin >> datamu[4];
+    cout << endl;
+
+    //Menampilkan data dengan perintah satu persatu
+    cout << "data pertama = " << datamu[0] << endl;
+    cout << "data kedua = " << datamu[1] << endl;
+    cout << "data ketiga = " << datamu[2] << endl;
+    cout << "data keempat = " << datamu[3] << endl;
+    cout << "data kelima = " << datamu[4] << endl;
+    cout << endl;
+
+    
 }
